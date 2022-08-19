0aaed691a7adafd7328186a0b2abc0287aba5e23
Sam Friedman <sam.friedman@gmail.com>
2022-08-19 13:42:44 -0400

    Update KeepersDemo.sol

--- KeepersDemo.sol
+++ KeepersDemo.sol
@@ -2,17 +2,24 @@
 pragma solidity ^0.8.7;
 
 contract KeepersSample  {
+    function batchReveal() external returns (bool success) { }
     function compoundVault() external returns (bool success) { }
     function compoundVaultId(uint vaultId) external returns (bool success) { }
-    function rebaseToken() external returns (bool success) { }
-    function startNextRound() external returns (bool success) { }
     function distributeRewards() external returns (bool success) { }
     function distributeFees() external returns (bool success) { }
-    function startLottery() external returns (bool success) { }
-    function startRaffle() external returns (bool success) { }
     function rebalanceIndex() external returns (bool success) { }
     function rebalanceIndexId(uint indexId) external returns (bool success) { }
     function rebalancePositionId(uint indexId) external returns (bool success) { }
-    function updateTWAP() external returns (bool success) { }  
+    function refreshLeaderboard() external returns (bool success) { }
     function refreshBoxScores() external returns (bool success) { }  
+    function requestCancelRentNFT() external returns (bool success) { }
+    function rebaseToken() external returns (bool success) { }
+    function startAuction() external returns (bool success) { }
+    function startLottery() external returns (bool success) { }
+    function startMint() external returns (bool success) { }
+    function startNextRound() external returns (bool success) { }
+    function startRaffle() external returns (bool success) { }
+    function swapAndTop() external returns (bool success) { }
+    function topUp() external returns (bool success) { }
+    function updateTWAP() external returns (bool success) { }  
 }
